45e666e53d43b4507f46731a286b03555f28d184
K <Akarfire@gmail.com>
2026-02-13 14:26:55 +0300

    [DEV] Better Nested State Machine State creation function

--- Source/ScarletStateMachines/Private/ScarletStateMachines_Utilities.cpp
+++ Source/ScarletStateMachines/Private/ScarletStateMachines_Utilities.cpp
@@ -2,7 +2,6 @@
 
 
 #include "ScarletStateMachines_Utilities.h"
-#include "SSM_NestedStateMachineState.h"
 
 // Creates a new state machine of a given class
 USSM_StateMachine* UScarletStateMachines_Utilities::CreateStateMachine(TSubclassOf<USSM_StateMachine> StateMachineClass, UObject* Owner, bool AutoInit)
@@ -16,20 +15,25 @@ USSM_StateMachine* UScarletStateMachines_Utilities::CreateStateMachine(TSubclass
 }
 
 // Constructs a nested state machine state with EmbeddedStateMachineClass inside of it and then adds this state to the StateMachine
-void UScarletStateMachines_Utilities::AddNestedStateMachineState(USSM_StateMachine* StateMachine, uint8 InNewStateID, TSubclassOf<USSM_StateMachine> EmbeddedStateMachineClass)
+USSM_StateMachine* UScarletStateMachines_Utilities::AddNestedStateMachineState(USSM_StateMachine* StateMachine, uint8 InNewStateID, 
+    TSubclassOf<USSM_StateMachine> EmbeddedStateMachineClass, TSubclassOf<USSM_NestedStateMachineState> NestedStateMachineStateClass, bool AutoInit)
 {
-    if (!StateMachine) return;
-    if (!EmbeddedStateMachineClass) return;
+    if (!StateMachine) return nullptr;
+    if (!EmbeddedStateMachineClass) return nullptr;
 
     // Creating a nested state with it's embedded state machine
     
-    USSM_NestedStateMachineState* NewState = NewObject<USSM_NestedStateMachineState>(StateMachine, USSM_NestedStateMachineState::StaticClass());
+    USSM_NestedStateMachineState* NewState = NewObject<USSM_NestedStateMachineState>(StateMachine, NestedStateMachineStateClass);
     
     USSM_StateMachine* NewStateMachine = NewObject<USSM_StateMachine>(NewState, EmbeddedStateMachineClass);
-    NewStateMachine->InitStateMachine();
+
+    if (AutoInit)
+        NewStateMachine->InitStateMachine();
 
     NewState->SetEmbeddedStateMachine(NewStateMachine);
 
     // Adding nested state to the specified state machine
     StateMachine->AddNewStateExisting(InNewStateID, NewState);
+
+    return NewStateMachine;
 }
--- Source/ScarletStateMachines/Public/ScarletStateMachines_Utilities.h
+++ Source/ScarletStateMachines/Public/ScarletStateMachines_Utilities.h
@@ -5,6 +5,7 @@
 #include "CoreMinimal.h"
 #include "Kismet/BlueprintFunctionLibrary.h"
 #include "SSM_StateMachine.h"
+#include "SSM_NestedStateMachineState.h"
 #include "ScarletStateMachines_Utilities.generated.h"
 
 /**
@@ -20,6 +21,10 @@ class SCARLETSTATEMACHINES_API UScarletStateMachines_Utilities : public UBluepri
 	static USSM_StateMachine* CreateStateMachine(TSubclassOf<USSM_StateMachine> StateMachineClass, UObject* Owner, bool AutoInit = true);
 
 	// Constructs a nested state machine state with EmbeddedStateMachineClass inside of it and then adds this state to the StateMachine
+	// Returns a pointer to the embedded state machine
 	UFUNCTION(BlueprintCallable, Category = "ScarletStateMachines")
-	static void AddNestedStateMachineState(USSM_StateMachine* StateMachine, uint8 InNewStateID, TSubclassOf<USSM_StateMachine> EmbeddedStateMachineClass);
+	static USSM_StateMachine* AddNestedStateMachineState(	USSM_StateMachine* StateMachine, uint8 InNewStateID,
+															TSubclassOf<USSM_StateMachine> EmbeddedStateMachineClass,
+															TSubclassOf<USSM_NestedStateMachineState> NestedStateMachineStateClass,
+															bool AutoInit = true);
 };
